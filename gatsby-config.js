e0d0f3066678a6c9483529c312e2c6bb7abea650
Favour George <phavorsparks@gmail.com>
2020-11-25 10:32:25 +0100

    Add layout files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Portfolio | George Favour`,
+    description: `Software Engineer, Tech Advocate, Web technology Instructor`,
+    author: `@georgefavourc`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
